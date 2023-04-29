7e0044f8b728bc82911490395d9bed1df7a49dc2
Christos Ploutarcou <cploutarchou@gmail.com>
2023-04-29 13:21:36 +0300

    Refactor procucer package. Update producer tests

--- producer/procucer.go
+++ producer/procucer.go
@@ -1,11 +1,11 @@
 package producer
 
 import (
-	"context"
 	"errors"
-	"fmt"
-	"github.com/segmentio/kafka-go"
+	"github.com/Shopify/sarama"
 	"go-kafka/types"
+	"log"
+	"os"
 )
 
 type Producer interface {
@@ -14,12 +14,12 @@ type Producer interface {
 	Close() error
 }
 
-type Impl struct {
-	writer *kafka.Writer
+type producerImpl struct {
+	producer sarama.SyncProducer
+	Config   *types.WriterConfig
 }
 
 func NewProducer(brokers []string, topic string, config *types.WriterConfig) (Producer, error) {
-	fmt.Println(topic)
 	if len(brokers) == 0 {
 		return nil, errors.New("at least one broker is required")
 	}
@@ -38,36 +38,50 @@ func NewProducer(brokers []string, topic string, config *types.WriterConfig) (Pr
 		config.Topic = topic
 	}
 
-	writer := kafka.Writer{
-		Addr:                   kafka.TCP(config.Addr...),
-		Topic:                  config.Topic,
-		AllowAutoTopicCreation: config.AllowAutoTopicCreation,
+	if config.Logger == nil {
+		config.Logger = log.New(os.Stderr, "", log.LstdFlags)
+	}
+	saramaConfig := sarama.NewConfig()
+	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
+	saramaConfig.Producer.Retry.Max = 3
+	saramaConfig.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(config.Addr, saramaConfig)
+	if err != nil {
+		return nil, err
 	}
 
-	return &Impl{
-		writer: &writer,
+	return &producerImpl{
+		producer: producer,
+		Config:   config,
 	}, nil
-
 }
 
-func (k *Impl) Send(topic string, key []byte, value []byte) error {
-	k.writer.Async = false
-	k.writer.Topic = topic
-	return k.writer.WriteMessages(context.Background(), kafka.Message{
-		Key:   key,
-		Value: value,
-	})
+func (p *producerImpl) Send(topic string, key []byte, value []byte) error {
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.ByteEncoder(key),
+		Value: sarama.ByteEncoder(value),
+	}
+	partition, offset, err := p.producer.SendMessage(msg)
+	if err != nil {
+		p.Config.Logger.Printf("Failed to send message to topic %s: %s", topic, err)
+		return err
+	}
+	p.Config.Logger.Printf("Message sent to topic %s, partition %d, offset %d", topic, partition, offset)
+	return nil
 }
 
-func (k *Impl) SendAsync(topic string, key []byte, value []byte) error {
-	k.writer.Async = true
-	k.writer.Topic = topic
-	return k.writer.WriteMessages(context.Background(), kafka.Message{
-		Key:   key,
-		Value: value,
-	})
+func (p *producerImpl) SendAsync(topic string, key []byte, value []byte) error {
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.ByteEncoder(key),
+		Value: sarama.ByteEncoder(value),
+	}
+	p.producer.SendMessages([]*sarama.ProducerMessage{msg})
+	return nil
 }
 
-func (k *Impl) Close() error {
-	return k.writer.Close()
+func (p *producerImpl) Close() error {
+	return p.producer.Close()
 }
--- producer/procucer_test.go
+++ producer/procucer_test.go
@@ -28,7 +28,7 @@ func TestNewProducer(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewProducer([]string{"192.168.88.50:90920"}, "test", tt.args.config)
+			got, err := NewProducer([]string{"localhost:9092"}, "test", tt.args.config)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -65,7 +65,7 @@ func TestImpl_Send(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewProducer([]string{"192.168.88.50:9092"}, "test", tt.args.config)
+			got, err := NewProducer([]string{"localhost:9092"}, "test", tt.args.config)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -73,6 +73,47 @@ func TestImpl_Send(t *testing.T) {
 			// send 100 message in loop to test
 			for i := 0; i < 10; i++ {
 				err := got.Send("test", []byte(fmt.Sprintf("test message %d", i)), []byte(fmt.Sprintf("test message %d", i)))
+				fmt.Println("send message ", i)
+				if err != nil {
+					t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+			}
+			if err := got.Close(); err != nil {
+				t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println("send success")
+		})
+	}
+}
+
+func TestImpl_AsyncSend(t *testing.T) {
+	type args struct {
+		config *types.WriterConfig
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Producer
+		wantErr bool
+	}{
+		{
+			name:    "TestNewProducer",
+			args:    args{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewProducer([]string{"localhost:9092"}, "test", tt.args.config)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			time.Sleep(5 * time.Second)
+			// send 100 message in loop to test
+			for i := 0; i < 100; i++ {
+				err := got.SendAsync("test", []byte(fmt.Sprintf("test message %d", i)), []byte(fmt.Sprintf("test message %d", i)))
 				fmt.Println("send message ", i)
 				if err != nil {
 					t.Errorf("NewProducer() error = %v, wantErr %v", err, tt.wantErr)
