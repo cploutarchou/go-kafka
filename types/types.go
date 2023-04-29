bcda4acd39cfc521c6697ba5c71321fc4500f034
Christos Ploutarcou <cploutarchou@gmail.com>
2023-04-29 13:39:10 +0300

    Refactor Config Struct

--- types/types.go
+++ types/types.go
@@ -1,15 +1,18 @@
 package types
 
 import (
-	"github.com/segmentio/kafka-go"
+	"fmt"
 	"log"
+	"math"
 	"time"
+
+	"github.com/Shopify/sarama"
 )
 
-type WriterConfig struct {
-	Addr                   []string
+type Config struct {
+	Brokers                []string
 	Topic                  string
-	Balancer               kafka.Balancer
+	BalanceStrategy        string
 	MaxAttempts            int
 	WriteBackoffMin        time.Duration
 	WriteBackoffMax        time.Duration
@@ -18,11 +21,46 @@ type WriterConfig struct {
 	BatchTimeout           time.Duration
 	ReadTimeout            time.Duration
 	WriteTimeout           time.Duration
-	RequiredAcks           int
+	RequiredAcks           int16
 	Async                  bool
-	Compression            kafka.CompressionCodec
+	Compression            string
 	Logger                 *log.Logger
 	ErrorLogger            *log.Logger
-	Transport              *kafka.Transport
 	AllowAutoTopicCreation bool
+}
+
+func (c *Config) saramaConfig() (*sarama.Config, error) {
+	config := sarama.NewConfig()
+
+	switch c.BalanceStrategy {
+	case "Range":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange
+	case "RoundRobin":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	case "Sticky":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
+	default:
+		return nil, fmt.Errorf("invalid balance strategy: %s", c.BalanceStrategy)
+	}
+
+	config.Producer.RequiredAcks = sarama.RequiredAcks(c.RequiredAcks)
+	config.Producer.Retry.Max = c.MaxAttempts
+	config.Producer.Retry.Backoff = c.WriteBackoffMin
+	config.Producer.Retry.BackoffFunc = func(retries, maxRetries int) time.Duration {
+		backoff := c.WriteBackoffMin * time.Duration(math.Pow(2, float64(retries)))
+		if backoff > c.WriteBackoffMax {
+			backoff = c.WriteBackoffMax
+		}
+		return backoff
+	}
+
+	config.Producer.Flush.Bytes = c.BatchBytes
+	config.Producer.Flush.Frequency = c.BatchTimeout
+	config.Producer.Flush.Messages = c.BatchSize
+	config.Net.ReadTimeout = c.ReadTimeout
+	config.Net.WriteTimeout = c.WriteTimeout
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.ClientID = "kafka-go"
+
+	return config, nil
 }
